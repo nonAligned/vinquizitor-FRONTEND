a325597f107f39435112ae4b46b65909bb5cf099
Momcilo Savic <savic227@gmail.com>
2023-09-18 21:10:07 +0200

    implemented basic menu on homepage with basic styling

--- src/app/core/homepage/homepage.component.ts
+++ src/app/core/homepage/homepage.component.ts
@@ -6,5 +6,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./homepage.component.scss']
 })
 export class HomepageComponent {
+  activeOption: string = "quiz";
+  options: Array<string> = ["quiz", "search", "random"];
 
+  navigate(direction: string) {
+    let activeOptionIndex = this.options.indexOf(this.activeOption);
+    if (direction === "left") {
+      if (activeOptionIndex === 0) {
+        this.activeOption = this.options[this.options.length-1];
+      } else {
+        this.activeOption = this.options[activeOptionIndex-1];
+      }
+    } else if (direction === "right") {
+      if (activeOptionIndex === this.options.length-1) {
+        this.activeOption = this.options[0];
+      } else {
+        this.activeOption = this.options[activeOptionIndex+1];
+      }
+    }
+  }
 }
