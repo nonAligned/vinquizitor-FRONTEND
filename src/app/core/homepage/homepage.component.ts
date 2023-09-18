2aac16954ee5137b9efc5ff0e1c1759dca875a75
Momcilo Savic <savic227@gmail.com>
2023-09-18 20:05:04 +0200

    Generated homepage component and added route

---
+++ src/app/core/homepage/homepage.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wine-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.scss']
+})
+export class HomepageComponent {
+
+}
