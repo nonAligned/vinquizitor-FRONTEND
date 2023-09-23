e1da70ee6ba4a7bb8a4d4f3f33c65881a86b30ac
Momcilo Savic <savic227@gmail.com>
2023-09-23 15:22:53 +0200

    Added routing for Contact component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './core/homepage/homepage.component';
+import { ContactComponent } from './core/contact/contact.component';
 
 const routes: Routes = [
   {path: "home", component: HomepageComponent},
+  {path: "contact", component: ContactComponent},
   {path: "", redirectTo: "/home", pathMatch: "full"}
 ];
 
