1b3c4bae72d5bb32b3cc2d9a49510c9e2d97955d
Momcilo Savic <savic227@gmail.com>
2023-09-25 11:14:25 +0200

    Added routing for search component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './core/homepage/homepage.component';
 import { ContactComponent } from './core/contact/contact.component';
+import { SearchComponent } from './wine/search/search.component';
 
 const routes: Routes = [
   {path: "home", component: HomepageComponent},
   {path: "contact", component: ContactComponent},
+  {path: "search", component: SearchComponent},
   {path: "", redirectTo: "/home", pathMatch: "full"}
 ];
 
