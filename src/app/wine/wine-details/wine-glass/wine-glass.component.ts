56bf055f440a51a9f98eef34130d1da5eb5f891e
Momcilo Savic <savic227@gmail.com>
2023-10-05 11:43:14 +0200

    FInished basic template and style for wine glass component

--- src/app/wine/wine-details/wine-glass/wine-glass.component.ts
+++ src/app/wine/wine-details/wine-glass/wine-glass.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'wine-wine-glass',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./wine-glass.component.scss']
 })
 export class WineGlassComponent {
+  @Input() activeGlass: string = "";
+
+  
 
 }
