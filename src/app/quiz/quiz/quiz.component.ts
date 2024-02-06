39773034733939fb6442d029faaf4c02bca4de69
Momcilo Savic <savic227@gmail.com>
2024-02-06 08:50:18 +0100

    Added a method in quiz service for fetching questions from API and in quiz component added a method that calls quiz service when component initializes

--- src/app/quiz/quiz/quiz.component.ts
+++ src/app/quiz/quiz/quiz.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuizQuestion } from 'src/app/models/quiz-question.model';
+import { QuizService } from 'src/app/services/quiz.service';
 
 @Component({
   selector: 'wine-quiz',
   templateUrl: './quiz.component.html',
   styleUrls: ['./quiz.component.scss']
 })
-export class QuizComponent {
+export class QuizComponent implements OnInit {
+  quizQuestions: QuizQuestion[] = [];
 
+  constructor(private quizService: QuizService) { }
+
+  ngOnInit(): void  {
+    this.getQuizQuestions();
+  }
+
+  getQuizQuestions(): void {
+    this.quizService.getQuizQuestions().subscribe(quizQuestions => {
+      for(let quizQuestion of quizQuestions.results) {
+        this.quizQuestions.push(quizQuestion);
+      }
+      console.log(this.quizQuestions);
+    });
+  }
 }
--- src/app/services/quiz.service.ts
+++ src/app/services/quiz.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { QuizQuestionList } from '../models/quiz-question-list.model';
+
+const BASE_URL = "http://localhost:5000/api";
 
 @Injectable({
   providedIn: 'root'
 })
 export class QuizService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getQuizQuestions(): Observable<QuizQuestionList> {
+    return this.http.get<QuizQuestionList>(BASE_URL + "/quiz/questions").pipe(map(data => {
+      return new QuizQuestionList(data);
+    }));
+  }
 }
