1c42657664df894d2bb7964df23ced535a2e1d72
Momcilo Savic <savic227@gmail.com>
2024-03-09 16:27:21 +0100

    Added responsive styles to navbar and homepage and swipe functionality for homepage menu

--- src/app/core/homepage/homepage.component.ts
+++ src/app/core/homepage/homepage.component.ts
@@ -13,6 +13,8 @@ export class HomepageComponent {
   buttonRight = faChevronCircleRight;
   activeOption: string = "quiz";
   options: Array<string> = ["quiz", "search", "random"];
+  swipeCoord?: [number, number];
+  swipeTime?: number;
 
   constructor(private quizService: QuizService, private router: Router) {}
 
@@ -37,5 +39,23 @@ export class HomepageComponent {
     this.quizService.getRandomResult().subscribe(randomResult => {
       this.router.navigateByUrl(`/search/${randomResult._id}`, {skipLocationChange: true});
     });
+  }
+
+  swipe(event: TouchEvent, when: string): void {
+    const coord: [number, number] = [event.changedTouches[0].clientX, event.changedTouches[0].clientY];
+    const time = new Date().getTime();
+
+    if (when === "start") {
+      this.swipeCoord = coord;
+      this.swipeTime = time;
+    } else if (when === "end") {
+      const direction = [coord[0] - this.swipeCoord![0], coord[1] - this.swipeCoord![1]];
+      const duration = time - this.swipeTime!;
+
+      if (duration < 1000 && Math.abs(direction[0]) > 30 && Math.abs(direction[0]) > Math.abs(direction[1] * 3)) {
+        const swipe = direction[0] < 0 ? "left" : "right";
+        this.navigate(swipe);
+      }
+    }
   }
 }
