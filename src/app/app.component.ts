95493c77a41cbc2233aa98e0bee7f243698eee84
Momcilo Savic <savic227@gmail.com>
2024-03-11 17:56:38 +0100

    Added responsive styles for contact and page not found components and added scroll to view on router outlet activation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'vinquizitor';
+
+  onRouterActivate(): void {
+    document.getElementById("router")?.scrollIntoView();
+  }
 }
